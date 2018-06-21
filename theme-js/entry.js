bf897c6545d0603862fedbeb7724017d6e830e52
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-06-20 22:17:13 -0700

    Initial commit with skeleton

---
+++ theme-js/entry.js
@@ -0,0 +1,12 @@
+// import the parent theme style
+import '../theme-styles/jsng-theme.scss'
+
+//import font-awesomestyles
+import fontawesome from '@fortawesome/fontawesome'
+import faFacebook from '@fortawesome/fontawesome-free-brands/faFacebook'
+import faLinkedinIn from '@fortawesome/fontawesome-free-brands/faLinkedinIn'
+
+fontawesome.library.add(faFacebook,faLinkedinIn)
+
+console.log('File has loaded');
+
