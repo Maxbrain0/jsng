2319ebbfbdeeeeb7d455168d3dff65875a9aa9fd
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-07-20 13:18:50 -0700

    Complete to menu button style for drop down nav

--- theme-js/entry.js
+++ theme-js/entry.js
@@ -1,14 +1,15 @@
 // import the parent theme style
 import '../theme-styles/jsng-theme.scss';
 
+// for toggling navigation
+import './navToggles';
+
 //import font-awesomestyles
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { faChevronCircleDown } from '@fortawesome/free-solid-svg-icons';
 
 
 // Add all icons to the library so you can use it in your page
-library.add(faAngleDown);
+library.add(faChevronCircleDown);
 dom.watch();
 
-// for toggling navigation
-// import './navToggles';
--- theme-js/navToggles.js
+++ theme-js/navToggles.js
@@ -1,2 +1,7 @@
 //provides script for dropdown of navigation
 
+const menuToggle = document.querySelector(".j-menu-toggle");
+
+menuToggle.addEventListener('click', () => {
+  console.log(menuToggle);
+})
