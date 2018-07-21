0e7e9e920ece4743f1ece983262e3fb4c61e97cd
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-07-20 21:40:28 -0700

    Add site-wide social navigations links

--- theme-js/entry.js
+++ theme-js/entry.js
@@ -4,10 +4,10 @@ import '../theme-styles/jsng-theme.scss';
 //import font-awesomestyles
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import { faChevronCircleDown } from '@fortawesome/free-solid-svg-icons';
-
+import { faFacebookSquare, faTwitterSquare, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 // Add all icons to the library so you can use it in your page
-library.add(faChevronCircleDown);
+library.add(faChevronCircleDown, faFacebookSquare, faTwitterSquare, faLinkedin, faGithub);
 dom.watch();
 
 // for toggling navigation
