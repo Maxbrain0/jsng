e9e9397f5cc0e11af66b6c470542fe5d17123962
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-07-27 08:55:59 -0700

    Style index page pagination links

--- theme-js/entry.js
+++ theme-js/entry.js
@@ -3,11 +3,12 @@ import '../theme-styles/jsng-theme.scss';
 
 //import font-awesomestyles
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import { faChevronCircleDown } from '@fortawesome/free-solid-svg-icons';
+import { faChevronCircleDown, faChevronCircleLeft, faChevronCircleRight } from '@fortawesome/free-solid-svg-icons';
 import { faFacebookSquare, faTwitterSquare, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 // Add all icons to the library so you can use it in your page
-library.add(faChevronCircleDown, faFacebookSquare, faTwitterSquare, faLinkedin, faGithub);
+library.add(faChevronCircleDown, faChevronCircleRight, faChevronCircleLeft, 
+  faFacebookSquare, faTwitterSquare, faLinkedin, faGithub);
 dom.watch();
 
 // for toggling navigation
