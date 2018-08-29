22c435f5894e5f725f3d3bfa33f4415b802921d5
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-08-29 11:13:39 -0700

    Add noframe library for embedded content to fit parent

--- theme-js/entry.js
+++ theme-js/entry.js
@@ -6,6 +6,9 @@ import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import { faChevronCircleDown, faChevronCircleLeft, faChevronCircleRight } from '@fortawesome/free-solid-svg-icons';
 import { faFacebookSquare, faTwitterSquare, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
+// import reframe for making embed videos full column width
+import noframe from 'reframe.js/dist/noframe.js';
+
 // Add all icons to the library so you can use it in your page
 library.add(faChevronCircleDown, faChevronCircleRight, faChevronCircleLeft, 
   faFacebookSquare, faTwitterSquare, faLinkedin, faGithub);
@@ -13,3 +16,6 @@ dom.watch();
 
 // for toggling navigation
 import './navToggles';
+
+// reframe embeds
+noframe('iframe','figure');
